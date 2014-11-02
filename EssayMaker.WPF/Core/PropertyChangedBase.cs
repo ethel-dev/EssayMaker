3a58df3867cbaf2bbc1e22251f69f520563edb37
sargeant45 <ethanarterberry@gmail.com>
2014-11-01 18:39:17 -0700

    Organized all files. Removed not working folders and useless folders.

---
+++ EssayMaker.WPF/Core/PropertyChangedBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+
+namespace EssayMaker.Core
+{
+    [DataContract]
+    public class PropertyChangedBase:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void NotifyPropertyChange(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void NotifyPropertyChange<T>(Expression<Func<T>> expr)
+        {
+            var lambda = (LambdaExpression)expr;
+
+            MemberExpression memberExpression;
+            if (lambda.Body is UnaryExpression)
+            {
+                var unaryExpression = (UnaryExpression)lambda.Body;
+                memberExpression = (MemberExpression)unaryExpression.Operand;
+            }
+            else
+                memberExpression = (MemberExpression)lambda.Body;
+
+            NotifyPropertyChange(memberExpression.Member.Name);
+        }
+    }
+}
