a7dc3cced3fe7c6c39ef5174615aae0332ecfd96
Josh Trommel <joshtrommel@gmail.com>
2015-12-17 00:18:51 -0800

    First shell->gulp conversion wave

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,42 @@ var wiredep = require('wiredep').stream;
 var uglify = require('gulp-uglify');
 var minifyCss = require('gulp-minify-css');
 var htmlmin = require('gulp-htmlmin');
+var coffee = require('gulp-coffee');
+var bower = require('gulp-bower');
 
 // these are default tasks that are done all of the time
-gulp.task('default', function() {
+gulp.task('default', ['wiredep']);
+
+// setup task for initialization
+gulp.task('setup', ['bower', 'coffee', 'wiredep'])
+
+// watches coffee task for changes
+gulp.task('watch', function() {
+  gulp.watch('src/coffee/*.coffee', ['coffee']);
+})
+
+// set up wiredep with index.html
+gulp.task('wiredep', function() {
   gulp.src('index.html')
     .pipe(wiredep({
       optional: 'configuration',
       goes: 'here'
     }))
     .pipe(gulp.dest(''));
-});
+})
+
+// compile coffee to js
+gulp.task('coffee', function() {
+  return gulp.src('src/coffee/*.coffee')
+    .pipe(coffee({bare:true}))
+    .pipe(gulp.dest('src/coffee/js'));
+})
+
+// just installs bower dependencies. gotta have a gulp task for everything
+gulp.task('bower', function() {
+  return bower()
+    .pipe(gulp.dest('lib/'));
+})
 
 // this is only run when distributing essaymaker
 gulp.task('distribute', function() {
